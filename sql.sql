10a44cf4794be5d2ca0099e3fdd545f07b26e902
MrKrators <guionpiso@gmail.com>
2023-05-29 01:57:22 -0500

    update sql

--- sql.sql
+++ sql.sql
@@ -1,56 +1,167 @@
--- -----------------------------------------------------
--- Schema trabajofinal-lb4
--- -----------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-05-2023 a las 08:55:29
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.0
 
--- -----------------------------------------------------
--- Schema trabajofinal-lb4
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `trabajofinal-lb4` DEFAULT CHARACTER SET utf8 ;
-USE `trabajofinal-lb4` ;
-
--- -----------------------------------------------------
--- Table `trabajofinal-lb4`.`categoria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trabajofinal-lb4`.`categoria` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `trabajofinal-lb4`.`producto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trabajofinal-lb4`.`producto` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `detalle` VARCHAR(45) NULL,
-  `categoria_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_producto_categoria1_idx` (`categoria_id` ASC),
-  CONSTRAINT `fk_producto_categoria1`
-    FOREIGN KEY (`categoria_id`)
-    REFERENCES `trabajofinal-lb4`.`categoria` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `trabajofinal-lb4`
+--
 
--- -----------------------------------------------------
--- Table `trabajofinal-lb4`.`item`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trabajofinal-lb4`.`item` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `precio` INT NOT NULL,
-  `stock` INT NOT NULL,
-  `producto_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_item_producto_idx` (`producto_id` ASC),
-  CONSTRAINT `fk_item_producto`
-    FOREIGN KEY (`producto_id`)
-    REFERENCES `trabajofinal-lb4`.`producto` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `categoria`
+--
 
+CREATE TABLE `categoria` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nombre`) VALUES
+(1, 'Pantalon'),
+(2, 'Camisa'),
+(3, 'Camisetas'),
+(4, 'Rave'),
+(5, 'Zapatos'),
+(6, 'Calcetines'),
+(7, 'Lentes');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `item`
+--
+
+CREATE TABLE `item` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `precio` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `producto_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `item`
+--
+
+INSERT INTO `item` (`id`, `precio`, `stock`, `producto_id`) VALUES
+(1, 15500, 11, 1),
+(2, 30500, 2, 2),
+(3, 80000, 65, 3),
+(4, 95300, 4, 4),
+(5, 33000, 0, 5),
+(6, 45200, 3, 6),
+(7, 10000, 44, 7),
+(8, 99500, 21, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `detalle` varchar(45) DEFAULT NULL,
+  `categoria_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `nombre`, `detalle`, `categoria_id`) VALUES
+(1, 'nike', 'rojo celeste', 1),
+(2, 'adidas', 'violeta XL', 2),
+(3, 'apple', 'negra M', 1),
+(4, 'polo', 'negro XXL', 3),
+(5, 'nike', 'red', 1),
+(6, 'adidas', 'blue', 2),
+(7, 'puma', 'green', 3),
+(8, 'casaca', 'roja xxxl', 4);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_item_producto_idx` (`producto_id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_producto_categoria1_idx` (`categoria_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `item`
+--
+ALTER TABLE `item`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `fk_item_producto` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `fk_producto_categoria1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
